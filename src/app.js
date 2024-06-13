76770b115e32b1d3688cfaefd944f91b20e723d3
PABLIUXDEV <167660504+PABLIUXDEV@users.noreply.github.com>
2024-06-13 06:36:59 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,35 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let who = ["My dad", "My mom", "The dog", "My sister", "My brother"];
+  let action = ["was using", "was eating", "crushed", "broke"];
+  let what = ["my laptop", "my television", "the ipad"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  console.log("Hello Rigo");
+  document.querySelector("#excuse").innerHTML =
+    getRandom(who) +
+    " " +
+    getRandom(action) +
+    " " +
+    getRandom(what) +
+    " " +
+    getRandom(when);
 };
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getRandom(anyArray) {
+  let max = anyArray.length - 1;
+  let min = 0;
+  let random = getRandomNumber(min, max);
+  return anyArray[random];
+}
