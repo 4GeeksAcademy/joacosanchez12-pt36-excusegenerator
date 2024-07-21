625889ddfaa950facefa9bf4f076bb7da94f103b
Pablo Dávila <167660504+PABLIUXDEV@users.noreply.github.com>
2024-07-21 23:35:25 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,34 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function aleatorio(lista) {
+    let numAleatorio = Math.floor(Math.random() * lista.length);
+    return lista[numAleatorio] + " ";
+  }
+
+  let who = ["Chuck Norris", "Mi abuela", "Jane Fonda", "Mi perro Ricky"];
+  let action = ["se comió", "destruyó", "aventó por la ventana", "destruyó"];
+  let what = ["mi tarea", "mi carro", "mi televisión"];
+  let when = [
+    "antes de clase",
+    "mientras dormía",
+    "cuando estaba fuera",
+    "durante mis clases de pilates",
+    "mientras atendía una llamada"
+  ];
+
+  let excuse =
+    aleatorio(who) + aleatorio(action) + aleatorio(what) + aleatorio(when);
+
+  let elementExcuse = document.querySelector("#excuse");
+
+  elementExcuse.innerHTML = excuse;
+
+  let excuseButton = document.querySelector("#generate-excuse");
+
+  excuseButton.addEventListener("click", () => {
+    let newExcuse =
+      aleatorio(who) + aleatorio(action) + aleatorio(what) + aleatorio(when);
+    elementExcuse.innerHTML = newExcuse;
+  });
 };
