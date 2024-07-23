92307b7bec0598fd095af86dd72ee7976e9da2f5
pattynouelc <141960475+pattynouelc@users.noreply.github.com>
2024-07-23 11:08:47 +0000

    guardando el código por primera vez

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  const who = ["El gato", "Mi mamá", "El encargado", "Mi novio"];
+  const action = ["comió", "orinó", "aplastó", "rompió"];
+  const what = ["mi tarea", "mi teléfono", "mi compu"];
+  const when = [
+    "antes de la clase",
+    "cuando yo estaba durmiendo",
+    "mientras cocinaba",
+    "durante la comida",
+  ];
+
+  function getExcuse() {
+    const whoIndex = Math.floor(Math.random() * who.length);
+    const actionIndex = Math.floor(Math.random() * action.length);
+    const whatIndex = Math.floor(Math.random() * what.length);
+    const whenIndex = Math.floor(Math.random() * when.length);
+
+    return `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`;
+  }
+
+  document.getElementById("excuse").innerHTML = getExcuse();
   console.log("Hello Rigo from the console!");
 };
