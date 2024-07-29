1d71649c1a4c033fddabf02fe9d1c6fb683c4e90
juanruizl <160032371+juanruizl@users.noreply.github.com>
2024-07-29 08:04:08 +0000

    Second commit

--- src/app.js
+++ src/app.js
@@ -1,14 +1,29 @@
 // eslint-disable-next-line;
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  let who = ["The dog", "My grandma", "The mailman", "My bird"];
-  let action = ["ate", "peed", "crushed", "broke"];
-  let what = ["my homework", "my phone", "the car"];
-  let when = [
+// Genera una excusa aleatoria
+function generateExcuse(whoArray, actionArray, whatArray, whenArray) {
+  const whoIndex = Math.floor(Math.random() * whoArray.length);
+  const actionIndex = Math.floor(Math.random() * actionArray.length);
+  const whatIndex = Math.floor(Math.random() * whatArray.length);
+  const whenIndex = Math.floor(Math.random() * whenArray.length);
+
+  return `${whoArray[whoIndex]} ${actionArray[actionIndex]} ${whatArray[whatIndex]} ${whenArray[whenIndex]}`;
+}
+
+// Muestra la excusa generada en el elemento HTML
+function displayExcuse(excuse) {
+  document.getElementById("excuse").innerText = excuse;
+}
+
+// Función principal que se ejecuta al cargar la página
+function main() {
+  const who = ["The dog", "My grandma", "The mailman", "My bird"];
+  const action = ["ate", "peed", "crushed", "broke"];
+  const what = ["my homework", "my phone", "the car"];
+  const when = [
     "before the class",
     "when I was sleeping",
     "while I was exercising",
@@ -16,20 +31,21 @@ window.onload = function() {
     "while I was praying"
   ];
 
-  function generateExcuse() {
-    let whoIndex = Math.floor(Math.random() * who.length);
-    let actionIndex = Math.floor(Math.random() * action.length);
-    let whatIndex = Math.floor(Math.random() * what.length);
-    let whenIndex = Math.floor(Math.random() * when.length);
+  try {
+    const initialExcuse = generateExcuse(who, action, what, when);
+    displayExcuse(initialExcuse);
 
-    return `${who[whoIndex]} ${action[actionIndex]} ${what[whatIndex]} ${when[whenIndex]}`;
+    // Agrega el evento para generar nuevas excusas
+    document
+      .getElementById("generateExcuseButton")
+      .addEventListener("click", () => {
+        const newExcuse = generateExcuse(who, action, what, when);
+        displayExcuse(newExcuse);
+      });
+  } catch (error) {
+    console.error("Error generating excuse:", error);
   }
+}
 
-  // Generar y mostrar la primera excusa al cargar la página
-  document.getElementById("excuse").innerHTML = generateExcuse();
-
-  // Función para generar y mostrar una nueva excusa
-  window.generateNewExcuse = function() {
-    document.getElementById("excuse").innerHTML = generateExcuse();
-  };
-};
+// Ejecuta la función principal cuando la ventana se ha cargado
+window.onload = main;
