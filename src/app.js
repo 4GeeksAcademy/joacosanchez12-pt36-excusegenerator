87ad85621084497c7aeefc00f48d83c40c38e1f0
Guillermo Obando <162076693+Sharguidev@users.noreply.github.com>
2024-08-05 02:49:04 +0000

    Initial Code

--- src/app.js
+++ src/app.js
@@ -5,7 +5,44 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+import "bootstrap";
+import "./style.css";
+
+import "./assets/img/rigo-baby.jpg";
+import "./assets/img/4geeks.ico";
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function excuseGenerator() {
+    let who = ["The dog", "my mom", "my father", "my granpa", "my grandma"];
+    let action = ["ate", "pooped", "crush", "took", "broke"];
+    let what = ["My laptop", "My keyboard", "My phone", "My house"];
+    let when = [
+      "before the class",
+      "After the class",
+      "when I was not home",
+      "during my lunch"
+    ];
+
+    const randomWho = Math.floor(Math.random() * who.length);
+    const randomAction = Math.floor(Math.random() * action.length);
+    const randomWhat = Math.floor(Math.random() * what.length);
+    const randomWhen = Math.floor(Math.random() * when.length);
+
+    const excuses =
+      who[randomWho] +
+      " " +
+      action[randomAction] +
+      " " +
+      what[randomWhat] +
+      " " +
+      when[randomWhen];
+
+    document.getElementById("excuse").textContent = excuses;
+  }
+
+  document
+    .getElementById("generate-button")
+    .addEventListener("click", excuseGenerator);
 };
