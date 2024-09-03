329560577a269933dc1ccb7fa2698147d093bce2
Andrés Arreaza <112995166+Andres-Arreaza@users.noreply.github.com>
2024-09-03 02:46:01 +0000

    Elimine comentarios, arregle la identacion, y agrege nuevos comentarios.

--- src/app.js
+++ src/app.js
@@ -12,17 +12,17 @@ let noun = ["jogger", "racoon"];
 window.onload = function() {
   const list = document.getElementById("list");
   let domains = [];
-  //Creacion de los for anidados.
+  //Creacion de los for anidados para recorrer los arrays.
   for (let i = 0; i < pronoun.length; i++) {
     for (let a = 0; a < adj.length; a++) {
       for (let b = 0; b < noun.length; b++) {
         let domain = pronoun[i] + adj[a] + noun[b] + ".com";
         domains.push(domain);
-        }
       }
     }
+  }
 
-  // Imprimir los dominios en el orden especificado
+  // Imprimir los dominios en el orden especificado.
   for (let i = 0; i < domains.length; i++) {
     list.innerHTML += `<li class="list-group-item">${domains[i]}</li>`; // Agregar al HTML
   }
