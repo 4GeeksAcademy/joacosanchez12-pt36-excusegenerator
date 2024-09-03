3face49ab1320caab872bd8f276c4657a511eced
Emma4212 <110198302+Emma4212@users.noreply.github.com>
2024-09-03 01:41:05 +0000

    Cambios 2.0

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// Define los arrays pronoun, adj y noun
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const list = document.getElementById("list");
+  // list.innerHTML = "Soy perra"; // Limpiar la lista antes de agregar los elementos
+
+  // Generar los dominios en el orden específico
+  let domains = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let a = 0; a < adj.length; a++) {
+      for (let b = 0; b < noun.length; b++) {
+        let domain = pronoun[i] + adj[a] + noun[b] + ".com";
+        domains.push(domain); // Almacenar el dominio en el array
+      }
+    }
+  }
+
+  // Imprimir los dominios en el orden especificado
+  for (let i = 0; i < domains.length; i++) {
+    list.innerHTML += `<li class="list-group-item">${domains[i]}</li>`; // Agregar al HTML
+  }
 };
