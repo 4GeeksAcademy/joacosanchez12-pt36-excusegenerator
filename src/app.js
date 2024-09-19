32b94aa3beaddf2512b541122daf27c34854c7d5
joacosanchez12 <joaquin.sancheezz@gmail.com>
2024-09-19 02:28:07 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+//array con cada elemento que se necesitará
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excuse").innerHTML = generateAnExcuse(
+    who,
+    action,
+    what,
+    when
+  );
 };
+
+function generateNumber(anArray) {
+  let num = Math.floor(Math.random() * anArray.length);
+  return num;
+}
+
+function generateAnExcuse(array1, array2, array3, array4) {
+  let newExcuse = "";
+  newExcuse = `${array1[generateNumber(array1)]} ${
+    array2[generateNumber(array2)]
+  } ${array3[generateNumber(array3)]} ${array4[generateNumber(array4)]}`;
+  console.log(newExcuse);
+  return newExcuse;
+}
